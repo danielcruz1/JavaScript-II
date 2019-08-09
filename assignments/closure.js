5bbb6cb522b488f82bf165ac72de2fd5049a5e8f
staccats <47672588+staccats@users.noreply.github.com>
2019-08-08 21:34:36 -0600

    closure.js complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,13 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+const lastName = 'Cruz';
+const bondGreeting = () => {
+  const firstName = "Daniel";
+  console.log(`The name's ${lastName}, ${firstName} ${lastName}.`);
+}
+
+bondGreeting();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
